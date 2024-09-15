a833451f2821b7fa4edf48be9d97d052bd219050
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-09-14 22:59:23 -0500

    Feat: add new file Reservation

---
+++ src/main/java/com/linktic/reservation_system/model/Reservation.java
@@ -0,0 +1,19 @@
+package com.linktic.reservation_system.model;
+
+import lombok.Data;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+    
+    private String reservationDetails;
+    private LocalDateTime reservationDate;
+}
